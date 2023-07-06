ae815bdfde8336135775bbbb14a8e15b003c28f3
Doğukan Biçer <dogukanbicer52@gmail.com>
2023-07-06 22:11:04 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CaylakMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Views/Home/Raspbian.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CaylakMvc.Views.Home
+{
+    public class RaspbianModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
